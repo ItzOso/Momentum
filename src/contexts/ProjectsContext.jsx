8f9ae7ec4bdafb9b8b6625ae95f82900cd7c852d
Oscar Ortiz <ortizoscjr@gmail.com>
2025-06-15 22:09:45 -0700

    Create ProjectsContext to fetch projects with onSnapshot real time

---
+++ src/contexts/ProjectsContext.jsx
@@ -0,0 +1,68 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useAuth } from "./AuthProvider";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../firebase/firebaseConfig";
+import toast from "react-hot-toast";
+
+const ProjectsContext = createContext();
+
+export const useProjects = () => {
+  return useContext(ProjectsContext);
+};
+
+export const ProjectsProvider = ({ children }) => {
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const { currentUser } = useAuth();
+
+  useEffect(() => {
+    if (!currentUser) {
+      setProjects([]);
+      setLoading(false);
+      return;
+    }
+
+    const q = query(
+      collection(db, "projects"),
+      where("uid", "==", currentUser.uid),
+      orderBy("updatedAt", "desc")
+    );
+
+    const unsubscribe = onSnapshot(
+      q,
+      (querySnapshot) => {
+        const userProjects = [];
+        querySnapshot.forEach((doc) => {
+          userProjects.push({ id: doc.id, ...doc.data() });
+        });
+        setProjects(userProjects);
+        setLoading(false);
+      },
+      (error) => {
+        console.error("Error fetching projects in real-time:", error);
+        toast.error("Failed to load projects.");
+        setLoading(false);
+      }
+    );
+
+    return () => unsubscribe();
+  }, []);
+
+  const value = {
+    projects,
+    loading,
+  };
+
+  return (
+    <ProjectsContext.Provider value={value}>
+      {children}
+    </ProjectsContext.Provider>
+  );
+};
